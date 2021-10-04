7e0068e55a76ba6b2411346ce0021ffe108ab47c
mariahdavalos <davism90@msu.edu>
2021-10-04 09:17:04 -0400

    why doesn't my save button work

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// proxy to get the content without throwing a CORS issue
+// proxy to get the content without throwing a CORS issue.
 const app = express();
 
 app.use('/getTrainSchedules', createProxyMiddleware({
